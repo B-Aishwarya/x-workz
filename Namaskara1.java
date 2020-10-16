ef4f50ee666be3370d917c67efcf10e3d1e9e3ee
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-16 10:01:08 +0530

    my First commit

---
+++ Namaskara1.java
@@ -0,0 +1,14 @@
+class Wish
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Namaskara");
+	  }
+}
+class Camera{
+public static void main(String args[])
+{
+System.out.println("Camera is Kodak");
+System.out.println("price is less");
+}
+}
