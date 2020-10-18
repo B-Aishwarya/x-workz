dbb084e3ebb660850dd85400cc00ac7852a062fe
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-18 19:11:09 +0530

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,24 @@
+class Food
+{
+	public static void main(String[] args)
+	{
+		System.out.println("North Indian");
+		Mexican();
+		south Indian();
+		Italian();
+		Chinease();
+		
+	}
+	public static void southIndian(){
+		System.out.println("south Indian");
+	}
+	public static void Italian(){
+		System.out.println("Italian");
+	}
+	public static void Chinease(){
+		System.out.println("Chinease");
+	}
+	public static void Mexican(){
+		System.out.println("Mexican");
+	}
+}
