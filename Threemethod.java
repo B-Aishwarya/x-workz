8edafcf16bb91c4d6cba00a5ef1d55c3bdd612cb
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-18 19:19:19 +0530

    Add files via upload

---
+++ Threemethod.java
@@ -0,0 +1,16 @@
+class Spoorti
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Kirti");
+		music();
+		drinking();
+		
+	}
+	public static void music(){
+		System.out.println("music");
+	}
+	public static void drinking(){
+		System.out.println("drinking");
+	}
+}
