652d9425111b326ccfff1e229335c187f16ecf22
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-18 19:16:44 +0530

    5th commit

---
+++ Writemethod.java
@@ -0,0 +1,13 @@
+class Writemethod
+{
+public static void main(String args[]){
+System.out.println("read");
+write();
+write();
+write();
+}
+public static void write()
+{
+	System.out.println("called write method 3 times");
+	}
+}
