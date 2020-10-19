8a7c33ab8c8f58bbebbb86a2773a1d8db788e140
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-19 21:27:25 +0530

    Add files via upload

---
+++ Datatypes3.java
@@ -0,0 +1,47 @@
+public class Datatypes3
+{
+	public static void main(String args[])
+	{  byte NumberOfMonths;
+		
+		short heightOfEverest;
+		heightOfEverest();
+		int microsofNetWorth;
+		microsofNetWorth();
+		long speciesInWorld;
+		speciesInWorld();
+		float gdpOfIndia;
+		gdpOfIndia();
+		double scientificReadings;
+		scientificReadings();
+		boolean petsAreCute;
+		petsAreCute();
+		char alphabet;
+		alphabet();
+		String game;
+		game();
+		}
+		 public static void heightOfEverest(){
+	  System.out.println("height Of Everest is 8848 m");
+	  }
+	   public static void microsofNetWorth(){
+	  System.out.println("micros of NetWorth is 143.02billion");
+	  }
+	   public static void speciesInWorld(){
+	  System.out.println("there are wide range of species In World");
+	  }
+	   public static void gdpOfIndia(){
+	  System.out.println("in 2018 gdp Of India was 2.72 lakh crores USD");
+	  }
+	   public static void scientificReadings(){
+	  System.out.println("scienticfic Readings are very accurate");
+	  }
+	   public static void petsAreCute(){
+	  System.out.println("pets Are Cute");
+	  }
+	   public static void alphabet(){
+	  System.out.println("starting letter of alphabet is  a");
+	  }
+	   public static void game(){
+	  System.out.println("pubg game is ban in India");
+	  }
+}
