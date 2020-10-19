c405058402f3284cb7a1c8dc48df7a2d57c3c2a6
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-19 21:23:13 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,51 @@
+public class Datatypes
+{
+	public static void main(String args[])
+	{  byte age;
+	    short daysInAYear;
+		int numberOfEngineersInIndia;
+		long populationOfIndia;
+		float percentageOfStudent;
+		double electron;
+		boolean earth;
+		char grade;
+		String name;
+		System.out.println("these are datatypes");
+		age();
+		days();
+		engineers();
+		population();
+		percentageOfStudent();
+		electron();
+		earth();
+		grade();
+		name();
+	}
+	  public static void age(){
+	  System.out.println("age of a tortise is more than a human");
+	  }
+	  public static void days(){
+	  System.out.println("days in a year are 365 days ");
+	  }
+	  public static void engineers(){
+	  System.out.println("every year more than 10lakhs  engineers come out of college in India");
+	  }
+	  public static void population(){
+	  System.out.println("In India population is increased day by day");
+	  }
+	  public static void percentageOfStudent(){
+	  System.out.println("we should not judge students based on their percentage");
+	  }
+	  public static void electron(){
+	  System.out.println("charge of a electron is 1.60217662*10^-19");
+	  }
+	  public static void earth(){
+	  System.out.println("earth is round");
+	  }
+	  public static void grade(){
+	  System.out.println("cbcs scheme is now introduced grade system for students");
+	  }
+	  public static void name(){
+	  System.out.println("my name is aishu");
+	  }
+}
