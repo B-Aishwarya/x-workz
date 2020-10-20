669f87664412c27d716ac01fb025e197cac495d6
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-20 19:50:33 +0530

    DatatypesTwo

---
+++ DatatypesTwo.java
@@ -0,0 +1,49 @@
+public class DatatypesTwo
+{
+	public static void main(String args[])
+	{  byte membersofKoko;
+		short costOfGold;
+		int ambaniHouse;
+		long bacteriaInHumanBody;
+		float speedOfWave;
+		double weightOfJupiter;
+		boolean sunRiseEveryDay;
+		char optionsInMcq;
+		String rules;
+		membersofKoko=12;
+		ambaniHouse=2000000000;
+		bacteriaInHumanBody=3900000;
+		speedOfWave=2.0f;
+		weightOfJupiter=1951000;
+		sunRiseEveryDay=true;
+		optionsInMcq='4';
+		rules="GovtRules";
+		costOfGold=25000;
+		
+		 
+	  System.out.println("there are "+membersofKoko);
+	 
+	  System.out.println("costOfGold per 5 grms " +costOfGold);
+	  
+	  
+	  System.out.println("total value of ambaniHouse is "+ambaniHouse);
+	  
+	  
+	  System.out.println(bacteriaInHumanBody+ "present in human body");
+	  
+	  System.out.println("speed Of Wave is "+ speedOfWave);
+	  
+	  System.out.println("weight Of Jupiter is"+ weightOfJupiter);
+	  
+	  
+	  System.out.println("every day sun rises"+sunRiseEveryDay);
+	 
+	  System.out.println(optionsInMcq+"optionsInMcq" );
+	  
+	  System.out.println("we should follow the " +rules);
+	  }
+}		
+		
+		
+		
+		
