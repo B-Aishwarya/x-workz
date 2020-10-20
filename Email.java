28c6b4d22474e52e091549370ba058a37b59f1f6
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-20 19:47:11 +0530

    Printing Email and programming language

---
+++ Email.java
@@ -0,0 +1,16 @@
+public class Email
+{
+	public static void main(String[] args){
+		System.out.println(" enter the email-id and prgramming language ");
+		emailId( "aishwaryasb777@gmail.com");
+		prgramming("java");
+	
+	}
+	public static void emailId(String emailId ){
+		System.out.println("email is "+ emailId);
+	}
+	public static void prgramming(String prgramming){
+		System.out.println("programming language is " + prgramming);
+	}
+	
+}
