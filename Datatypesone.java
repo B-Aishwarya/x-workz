cca13b98bb3519c094db11c141a6d03d695615ed
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-20 14:06:04 +0530

    Datatypesone

---
+++ Datatypesone.java
@@ -0,0 +1,40 @@
+public class Datatypesone
+{
+	public static void main(String args[])
+	{  byte ricePerKg;
+		short tab;
+		int numberOfForest;
+		long numberOfNerves;
+		float height;
+		double earthToMars;
+		boolean milkIsWhite;
+		char examBlocks;
+		String place;
+		System.out.println("these are datatypes");
+	  ricePerKg=100;
+	  System.out.println(ricePerKg+"  ricePerKg");
+	  tab=30000;
+	  System.out.println(tab+ "is minimum cost of tab ");
+	  
+	   numberOfForest=1000000000;
+	  System.out.println(numberOfForest+ " more than forest  in world");
+	  
+	   numberOfNerves=70000000;
+	  System.out.println("numberOfNerves in human is "+ numberOfNerves);
+	  
+	  height=2.5f;
+	  System.out.println("minimum heigh of female is "+ height);
+	  
+	   earthToMars=33900000;
+	  System.out.println("distance from ear to mars is ="+earthToMars);
+	  
+	  milkIsWhite=true;
+	  System.out.println("milkIsWhite is "+milkIsWhite);
+	  
+	  examBlocks= 'a';
+	  System.out.println("examBlocks are alloted according to roll numbers "+examBlocks);
+	  
+	  place= "udupi";
+	  System.out.println(" my native place is" +place);
+	  }
+}
