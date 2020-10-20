8df79faee52ef44dacb1450756aac7aaa8ac12f8
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-20 19:44:31 +0530

    FinalKeyword

---
+++ Final.java
@@ -0,0 +1,13 @@
+public class Final{ 
+   public static void main(String args[])
+   {
+        final String aadarNumber,phoneNumber,email;
+		 aadarNumber="628343803653"; 
+		 phoneNumber="9380957625";
+		 email="aishwarya77.xworkz@gmail.com";
+		 System.out.println("aadarNumber is "+ aadarNumber);
+		 System.out.println("phoneNumber is " + phoneNumber);
+		 System.out.println("email is "+ email);
+   } 
+		 
+   }
