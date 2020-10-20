9c726d8e9c63b4d63a844e9d807efd0a5af17245
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-20 19:43:11 +0530

    Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome
+{
+	public static void main(String[] args){
+		printWelcome("welcome");
+		
+	}
+	public static void printWelcome(String printWelcome)
+	{
+		System.out.println("hearthly "+ printWelcome);
+	}
+}
