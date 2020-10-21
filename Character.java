14a89b654d4f098f39036fc0d4c61ea5559f7f9f
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-21 20:04:30 +0530

    CharacterArray

---
+++ Character.java
@@ -0,0 +1,54 @@
+public class Character{
+	public static void main(String[] args){
+		System.out.println("characters");
+		charOfArray();
+	}
+	public static void charOfArray(){
+		char[] array= new char[10];
+		array[0]='a';
+		array[1]='b';
+		array[2]='c';
+		array[3]='d';
+		array[4]='e';
+		array[5]='f';
+		array[6]='g';
+		array[7]='h';
+		array[8]='i';
+	    char elementAtIndex1=array[0];
+		 
+		 System.out.println("Element at index one "+elementAtIndex1);
+		 
+		 char elementAtIndex2=array[1];
+		 
+		 System.out.println("Element at index two "+ elementAtIndex2);
+		 
+		 char elementAtIndex3=array[2];
+		 
+		 System.out.println("Element at index three"+ elementAtIndex3);
+		 char elementAtIndex4=array[3];
+		 
+		 System.out.println("Element at index four"+ elementAtIndex4);
+		 char elementAtIndex5=array[4];
+		 
+		 System.out.println("Element at index five"+ elementAtIndex5);
+		 char elementAtIndex6=array[5];
+		 
+		 System.out.println("Element at index six"+ elementAtIndex6);
+		 char elementAtIndex7=array[6];
+		 
+		 System.out.println("Element at index seven"+ elementAtIndex7);
+		char elementAtIndex8=array[7];
+		 
+		 System.out.println("Element at index eight"+ elementAtIndex8);
+		 char elementAtIndex9=array[8];
+		 
+		 System.out.println("Element at index nine"+elementAtIndex9);
+		char elementAtIndex10=array[9];
+		 
+		 System.out.println("Element at index ten"+elementAtIndex10);
+	
+	}
+	
+	
+	
+}
