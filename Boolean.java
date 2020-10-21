2564d725c8a4bc8d17f47940060f583e97a4f00c
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-21 20:02:46 +0530

    BooleanArray

---
+++ Boolean.java
@@ -0,0 +1,30 @@
+public class Boolean{
+	public static void main(String args[])
+	{
+		System.out.println("true or false:");
+		arrayOfMilk();
+	}
+	   public static void arrayOfMilk(){
+		   System.out.println("invoking arrayOf milk");
+		boolean milkIsWhite=true;
+		boolean milkIsBlack=false;
+		
+		boolean[] value={milkIsWhite,milkIsBlack};
+	    
+		
+		
+	     boolean elementAtIndex1=value[0];
+		 
+		 System.out.println("Element at index one "+elementAtIndex1);
+		 
+		 boolean elementAtIndex2=value[1];
+		 
+		 System.out.println("Element at index one "+elementAtIndex2);
+		 
+		
+	
+	}
+	
+	
+	
+}
