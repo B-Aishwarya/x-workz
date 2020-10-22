8e5572bd28ad29c6d0623b9e506b252516dc278d
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-10-22 19:44:32 +0530

    Add files via upload

---
+++ State.java
@@ -0,0 +1,33 @@
+public class State
+{
+	public static void main(String[] state)
+	{
+		System.out.println("India have 28 states   ");
+		
+		String statesArray="States";
+		statesName("Karnataka");
+		statesName("Punjab");
+		statesName("Kerala");
+		statesName("Maharastra");
+		statesName("Gujarat");
+		statesName("Delhi");
+		statesName("Tamilnadu");
+		statesName("Nagaland");
+		statesName("Uttar Pradesh");
+		statesName("Assam");
+		statesName("Jammu & Kashmir");
+		statesName("Bihar");
+		statesName("Andhra Pradesh");
+		statesName("Rajastan");
+		statesName("Odisha");
+		
+	}
+	public static void statesName(String name)
+	{
+		System.out.println("States are "+ name );		
+		
+		
+		
+		
+	}
+}
