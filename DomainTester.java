0faa950d73dcd087edd2d083702578b19538c363
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-11-10 19:53:55 +0530

    Add files via upload

---
+++ DomainTester.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class DomainTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		System.out.println("This is my first eclispe run");
+		String name="Aishu";
+		System.out.println(name);
+		System.out.println("Exit :: Main");
+		}
+	}
+
+
+
+
+
+
+	
