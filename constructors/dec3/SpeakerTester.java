7e2c1ddf7f754ec41eb14a8908ff2316248c176d
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-12-04 08:46:58 +0530

    Add files via upload

---
+++ constructors/dec3/SpeakerTester.java
@@ -0,0 +1,27 @@
+package com.xworkz.constructor.instancemethod;
+
+public class SpeakerTester {
+	public static void main(String[] args) {
+
+		Speaker speaker= new Speaker("RED", 6);
+		System.out.println(speaker.getCompanyName());
+		
+		System.out.println(speaker.isConnected());
+		
+		speaker.increaseVolume();
+		speaker.onOrOff();
+		speaker.increaseVolume();
+		speaker.increaseVolume();
+		speaker.increaseVolume();
+		speaker.increaseVolume();
+		speaker.increaseVolume();
+		
+		speaker.decreaseVolume();
+		speaker.decreaseVolume();
+		speaker.decreaseVolume();
+		speaker.decreaseVolume();
+		speaker.decreaseVolume();
+		
+
+}
+}
